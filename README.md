a4d3d628ede9ddd05720b8922f40bceaa092df0f
radendi <damasukamth@gmail.com>
2022-08-22 10:31:58 +0700

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+### Installation
+1. Clone this repo
+```
+git clone https://github.com/radendi/node-geolocation.git
+```
+2. Install requirement packages.
+```
+npm install
+```
+3. Run app
+```
+npm run app
+```
---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 5080
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/', (req, res) => {
+  console.table({
+    link: `https://maps.google.com/?q=${req.body.latitude},${req.body.longtitude}`,
+    browserInfo: req.body.browserInfo
+  })
+  res.send('success')
+})
+
+app.listen(port, () => {
+  console.log(`node-geolocation listening on port ${port}`)
+})
